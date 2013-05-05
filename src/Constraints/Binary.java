6314100bb2130d80a79ad559c5e252759b453754
julen <julen@debian>
2013-05-05 19:49:18 +0200

    Principal clases added

---
+++ src/Constraints/Binary.java
@@ -0,0 +1,21 @@
+package Constraints;
+
+import java.util.List;
+
+import es.deusto.ingenieria.is.csp.formulation.Constraint;
+import es.deusto.ingenieria.is.csp.formulation.Variable;
+
+public class Binary extends Constraint<Integer>{
+
+	public Binary(String arg0, List<Variable<Integer>> arg1) {
+		super(arg0, arg1);
+		// TODO Auto-generated constructor stub
+	}
+
+	@Override
+	public boolean isSatisfied(Variable<Integer> arg0, Integer arg1) {
+		// TODO Auto-generated method stub
+		return false;
+	}
+
+}
